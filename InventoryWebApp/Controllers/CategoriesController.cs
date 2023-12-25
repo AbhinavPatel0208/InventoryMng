fc1a3c02d741baffea42cf4b6aa0719a64d7e8d5
ABHINAV <abhinavpatel0208@gmail.com>
2023-12-25 13:26:14 +0530

    Update

---
+++ InventoryWebApp/Controllers/CategoriesController.cs
@@ -0,0 +1,20 @@
+﻿using InventoryWebApp.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace InventoryWebApp.Controllers
+{
+    public class CategoriesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Edit(int? id)
+        {
+            var category = new Category { CategoryId = id.HasValue ? id.Value : 0 };
+
+            return View(category);
+        }
+    }
+}
+/*49*/
