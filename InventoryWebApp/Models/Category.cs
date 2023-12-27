54e9e18674364a662a57b22847a0c274bb4fa2e0
ABHINAV <abhinavpatel0208@gmail.com>
2023-12-27 12:25:05 +0530

    Add Navbar To Project

--- InventoryWebApp/Models/Category.cs
+++ InventoryWebApp/Models/Category.cs
@@ -5,6 +5,5 @@
         public int CategoryId { get; set; }
         public string Name { get; set; } = string.Empty;
         public string Description { get; set; } = string.Empty;
-
-    }
+    }  
 }
